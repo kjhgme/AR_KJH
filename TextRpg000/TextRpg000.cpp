a657d1edf7286a8086010fb82f18b3decbb949d5
kjhgme <64467890+kjhgme@users.noreply.github.com>
2024-09-24 17:21:36 +0900

    TextRpg000_003
    SpeedCheck

--- TextRpg000/TextRpg000.cpp
+++ TextRpg000/TextRpg000.cpp
@@ -1,5 +1,12 @@
 ﻿#include <iostream>
 #include <conio.h>
+#include <random>
+using namespace std;
+
+random_device rd;
+default_random_engine dre(rd());
+uniform_int_distribution<int> uid(0, 10);
+
 
 const int LINECOUNT = 50;
 
@@ -7,10 +14,12 @@ const int NAMELEN = 10;
 
 int PlayerAtt = 0;
 int PlayerHp = 0;
+int PlayerSpeed = 10;
 char PlayerName[NAMELEN] = "NONE";
 
 int MonsterAtt = 10;
 int MonsterHp = 100;
+int MonsterSpeed = 10;
 char MonsterName[NAMELEN] = "NONE";
 
 void StrCopy(char* _Arr, int _BufferSize, const char* const _Name)
@@ -30,18 +39,20 @@ void StrCopy(char* _Arr, int _BufferSize, const char* const _Name)
 }
 
 // 행동
-void CreatePlayer(const char* const _Ptr, int _Att, int _Hp)
+void CreatePlayer(const char* const _Ptr, int _Att, int _Hp, int _Speed)
 {
     StrCopy(PlayerName, NAMELEN, _Ptr);
     PlayerAtt = _Att;
     PlayerHp = _Hp;
+    PlayerSpeed = _Speed;
 }
 
-void CreateMonster(const char* const _Ptr, int _Att, int _Hp)
+void CreateMonster(const char* const _Ptr, int _Att, int _Hp, int _Speed)
 {
     StrCopy(MonsterName, NAMELEN, _Ptr);
     MonsterAtt = _Att;
     MonsterHp = _Hp;
+    MonsterSpeed = _Speed;
 }
 
 void StatusRender(const char* _Name, int _Att, int _HP)
@@ -78,37 +89,53 @@ void MonsterStatusRender()
     StatusRender(MonsterName, MonsterAtt, MonsterHp);
 }
 
-void DamageLogic(const char* const _AttName, const char* const _DefName, int& _DefHp, int _Att)
+void DamageLogic(int& _DefHp, int _Att)
 {
     _DefHp -= _Att;
 }
 
-void DamageRender(const char* const _AttName, const char* const _DefName, int& _DefHp, int _Att)
+void DamageRender(const char* const _AttName, const char* const _DefName, int _Att)
 {
     printf_s("%s 가 %s를 공격해서 %d의 데미지를 입혔습니다.\n", _AttName, _DefName, _Att);
 }
 
-
 void Damage(const char* const _AttName, const char* const _DefName, int& _DefHp, int _Att)
 {
     // 게임 로직
-    DamageLogic(_AttName, _DefName, _DefHp, _Att);
+    DamageLogic(_DefHp, _Att);
 
     // 랜더링
-    DamageRender(_AttName, _DefName, _DefHp, _Att);
+    DamageRender(_AttName, _DefName, _Att);
 }
 
+void SpeedCheckRender(const char* const _Name)
+{
+    printf_s("%s 의 선공입니다\n", _Name);
+}
 
+void SpeedChecker(const char* const _PlayerName, const char* const _MonsterName, int _PlayerSpeed, int _MonsterSpeed)
+{
+    if (_PlayerSpeed > _MonsterSpeed)
+    {
+        SpeedCheckRender(_MonsterName);
+    }
+    else {
+        SpeedCheckRender(_PlayerName);
+    }
+}
+
+int SetRandomSpeed(int _Speed)
+{
+    _Speed = uid(dre);
+
+    return _Speed;
+}
 
 
 int main()
 {
-    // char Test0[100] = "Player";
-    /*char Test1[50] = Test0;
-    Test1 = Test0*/;
-
-    CreatePlayer("1", 10, 100);
-    CreateMonster("Orc", 10, 50);
+    CreatePlayer("1", 10, 100, PlayerSpeed);
+    CreateMonster("Orc", 10, 50, MonsterSpeed);
 
     PlayerStatusRender();
     MonsterStatusRender();
@@ -119,6 +146,9 @@ int main()
 
         char Input = ' ';
 
+        PlayerSpeed = SetRandomSpeed(PlayerSpeed);
+        MonsterSpeed = SetRandomSpeed(MonsterSpeed);
+
         PlayerStatusRender();
         MonsterStatusRender();
         Input = _getch();
@@ -127,27 +157,62 @@ int main()
         system("cls");
         PlayerStatusRender();
         MonsterStatusRender();
-        Damage(PlayerName, MonsterName, MonsterHp, PlayerAtt);
 
-        system("cls");
-        PlayerStatusRender();
-        MonsterStatusRender();
-        DamageRender(PlayerName, MonsterName, MonsterHp, PlayerAtt);
-
+        SpeedChecker(PlayerName, MonsterName, PlayerSpeed, MonsterSpeed);
         Input = _getch();
 
 
         system("cls");
-        PlayerStatusRender();
-        MonsterStatusRender();
-        Damage(MonsterName, PlayerName, PlayerHp, MonsterAtt);
+
+        if (PlayerSpeed > MonsterSpeed)
+        {
+            DamageLogic(PlayerHp, MonsterAtt);
+
+            PlayerStatusRender();
+            MonsterStatusRender();
+
+            SpeedCheckRender(MonsterName);
+
+            DamageRender(MonsterName, PlayerName, MonsterAtt);
+        }
+        else {
+            DamageLogic(MonsterHp, PlayerAtt);
+
+            PlayerStatusRender();
+            MonsterStatusRender();
+
+            SpeedCheckRender(PlayerName);
+
+            DamageRender(PlayerName, MonsterName, PlayerAtt);
+        }
+        Input = _getch();
+
 
         system("cls");
-        PlayerStatusRender();
-        MonsterStatusRender();
-        DamageRender(PlayerName, MonsterName, MonsterHp, PlayerAtt);
-        DamageRender(MonsterName, PlayerName, PlayerHp, MonsterAtt);
 
+        if (PlayerSpeed > MonsterSpeed)
+        {
+            DamageLogic(MonsterHp, PlayerAtt);
+
+            PlayerStatusRender();
+            MonsterStatusRender();
+
+            SpeedCheckRender(MonsterName);
+
+            DamageRender(MonsterName, PlayerName, MonsterAtt);
+            DamageRender(PlayerName, MonsterName, PlayerAtt);
+        }
+        else {
+            DamageLogic(PlayerHp, MonsterAtt);
+
+            PlayerStatusRender();
+            MonsterStatusRender();
+
+            SpeedCheckRender(PlayerName);
+
+            DamageRender(PlayerName, MonsterName, PlayerAtt);
+            DamageRender(MonsterName, PlayerName, MonsterAtt);
+        }
         Input = _getch();
     }
 
